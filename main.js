f31a8879ab465651ef7c6feab4f6a64dc77c7868
Kyle Shrote <shrotekyle@gmail.com>
2017-01-26 10:59:13 -0500

    Added Background Image

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     });
   });
 
-  for(i = 0; i < 225; i++){
+  for(i = 0; i < 256; i++){
       $('.container').append("<div class='square'></div>");
   }
   $('.square').hover(function(){
